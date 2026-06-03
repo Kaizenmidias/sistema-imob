a04607163f9506cbabaf2c427147d19801004cd9
Kaizenmidias <kaizenmidias@gmail.com>
2026-06-03 20:36:57 -0300

    Polyfill mb_split when mbstring is unavailable

--- public/index.php
+++ public/index.php
@@ -13,6 +13,17 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+if (!function_exists('mb_split')) {
+    function mb_split(string $pattern, string $string, int $limit = -1): array|false
+    {
+        $delimiter = '~';
+        $escaped = str_replace($delimiter, '\\' . $delimiter, $pattern);
+        $regex = $delimiter . $escaped . $delimiter . 'u';
+
+        return preg_split($regex, $string, $limit);
+    }
+}
+
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
